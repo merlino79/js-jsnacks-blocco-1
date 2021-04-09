f48719ca4e64d93541cbb9dd01091bbb568a7ae9
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-09 11:14:28 +0200

    riassegno cartelle

---
+++ js1/js/script.js
@@ -0,0 +1,16 @@
+var parola_1 = prompt("priama parola");
+console.log(parola_1.length);
+var parola_2 = prompt("seconda parola");
+console.log(parola_2.length);
+
+
+
+
+
+if (parola_1.length > parola_2.length) {
+    alert("parola 1 lunga");
+
+} else if (parola_2.length > parola_1.length) {
+    console.log("parola 2 piu lung")
+} else(parola_1.length = parola_2.length)
+console.log("sono ugulai");
