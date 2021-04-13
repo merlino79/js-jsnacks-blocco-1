e11845acf12291f8f320d8e74836da28336a0b09
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-13 11:53:56 +0200

    prova non riuscita nan

--- js3/js/script.js
+++ js3/js/script.js
@@ -1,5 +1,7 @@
 //creo la variabile per la somma
 
+var somma = 0 
+
 
 
 //creo un ciclo con 10 iterazioni dove chiedo i prompt per i dati
---
+++ js6/js/script.js
@@ -0,0 +1,20 @@
+//stampa il cubo dei primi n numeri dove n è un numero indicato dall'uente
+
+
+var numbers = 0;
+var numeri = prompt("chiedi un numero");
+
+console.log(numbers);
+var numbers = 0;
+
+
+for (i = 0; i < 6; i++) {
+    var num_utente = numbers[i];
+    console.log(numbers.lenght - 1);
+
+    var msgUscita = document.getElementById('lista').innerHTML;
+
+    document.getElementById('lista').innerHTML = msgUscita + "<li>" + " " + num_utente + "</li>";
+
+
+}
